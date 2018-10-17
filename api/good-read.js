f3692a64eafa62b1c681ed9cf44240d648c3cfa6
msutar <msutar@cimpress.com>
2018-10-17 17:33:41 +0530

    Update plugin of test

--- api/good-read.js
+++ api/good-read.js
@@ -3,7 +3,11 @@ const Client = require('node-rest-client').Client;
 const client = new Client();
 const searchBook = (text) => {
     return new Promise((resolve, reject) => {
+        if (!text) {
+            reject(new Error('Provide search text'));
+        }
         const url = `${process.env.GOOD_READ_URI}${process.env.GOOD_READ_SEARCH_URI}?key=${process.env.GOOD_READ_DEVELOPER_KEY}&q=${text}`;
+        console.log(url);
         client.get(url, {}, (data, response) => {
             if (Number(response.statusCode) === 200) {
                 let result = {};
@@ -12,8 +16,7 @@ const searchBook = (text) => {
                 }
                 resolve(result);
             } else {
-                reject(new Error("Error in good read api"));
-
+                reject(new Error("Error in good read API"));
             }
         });
     });
@@ -21,6 +24,9 @@ const searchBook = (text) => {
 
 const bookDetail = (bookId) => {
     return new Promise((resolve, reject) => {
+        if(!bookId){
+            reject(new Error('Provide book Id'));
+        }
         const url = `${process.env.GOOD_READ_URI}${process.env.GOOD_READ_BOOK_DETAIL}?key=${process.env.GOOD_READ_DEVELOPER_KEY}&id=${bookId}`;
         client.get(url, {}, (data, response) => {
             if (Number(response.statusCode) === 200) {
--- spec/api/good-read.spec.js
+++ spec/api/good-read.spec.js
@@ -1,16 +1,60 @@
-describe("Good Read API integration, ", () => {
-    const GooReadAPI = require('../../api/good-read');
-    it("should function to search book and get book details, ", () => {
-        expect(GooReadAPI.searchBook).toBeDefined();
-        expect(GooReadAPI.bookDetail).toBeDefined();
+describe("Good Read API integration,", () => {
+    const GoodReadAPI = require('../../api/good-read');
+    it("should function to search book and get book details,", () => {
+        expect(GoodReadAPI.searchBook).toBeDefined();
+        expect(GoodReadAPI.bookDetail).toBeDefined();
     });
 
     describe("searchBook funtion, ", () => {
-        it("should be an function", () => {
-            expect(typeof GooReadAPI.searchBook).toBe('function');
+        const underTest = GoodReadAPI.searchBook
+        it("should be an function.", () => {
+            expect(typeof underTest).toBe('function');
         });
-        it("should return a promise", () => {
-            expect()
+        it("should return a promise.", () => {
+            const goodRead = underTest().catch(() => { });
+            expect(typeof goodRead.then).toBe('function');
+            expect(typeof goodRead.catch).toBe('function');
+        });
+        it("should throw an error if a search text is not provided.", (done) => {
+            underTest()
+                .then(done.fail)
+                .catch(err => {
+                    expect(err.message).toBe('Provide search text');
+                    done();
+                });
+        });
+        describe("if search text is passed,", () => {
+            it('should return Error if Environment variable not defined', (done) => {
+                process.env.GOOD_READ_URI = "";
+                process.env.GOOD_READ_SEARCH_URI = "";
+                process.env.GOOD_READ_DEVELOPER_KEY = "";
+                underTest("test")
+                    .then(done.fail)
+                    .catch(err => {
+                        expect(err.message).toBe('Error in good read API')
+                        done();
+                    });
+            });
+        });
+    });
+
+    describe("bookDetail funtion, ", () => {
+        const underTest = GoodReadAPI.bookDetail
+        it("should be an function.", () => {
+            expect(typeof underTest).toBe('function');
+        });
+        it("should return a promise.", () => {
+            const goodRead = underTest().catch(() => { });
+            expect(typeof goodRead.then).toBe('function');
+            expect(typeof goodRead.catch).toBe('function');
+        });
+        it("should throw an error if a book id is not provided.", (done) => {
+            underTest()
+                .then(done.fail)
+                .catch(err => {
+                    expect(err.message).toBe('Provide book Id');
+                    done();
+                });
         });
     });
 });
